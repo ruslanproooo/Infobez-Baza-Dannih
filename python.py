2fc4ed5bfc7d652a6c74c64ad5a63fefb27c02d9
ruslanproooo <144116019+ruslanproooo@users.noreply.github.com>
2023-11-02 11:23:52 +0300

    Update python.py

--- python.py
+++ python.py
@@ -41,3 +41,38 @@ if a >= 18:
     print('Доступ разрешен')
 else:
     print('Доступ запрещен')
+  Наименьшее из двух чисел
+a = int(input())
+b = int(input())
+if a < b:
+    print(a)
+if b < a:
+    print(b)
+  Наименьшее из четырех чисел
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(min(a, b, c, d))
+  Возростная группа
+a = int(input())
+if a <= 13:
+    print('Детство')
+if a >=14 | a <= 24:
+    print('Молодость')
+if a >= 25 | a <= 59:
+    print('Зрелость')
+if a >= 60:
+    print('Старость')
+  Только +
+a = int(input())
+b = int(input())
+c = int(input())
+print((a if a > 0 else 0) + (b if b > 0 else 0) + (c if c > 0 else 0))
+  Красивое число
+a = int(input())
+if (1000 <= a <= 9999) and ( a % 7 == 0 or a % 17 == 0):
+    print('YES')
+else:
+    print("NO")
+  
