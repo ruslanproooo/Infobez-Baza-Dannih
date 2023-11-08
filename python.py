a11f1a6b351f0be50b1d05d976d878edee77ec1b
ruslanproooo <144116019+ruslanproooo@users.noreply.github.com>
2023-11-08 12:59:37 +0300

    Update python.py

--- python.py
+++ python.py
@@ -75,4 +75,45 @@ if (1000 <= a <= 9999) and ( a % 7 == 0 or a % 17 == 0):
     print('YES')
 else:
     print("NO")
-  
+                    2 модуль
+  Гонка спидстеров
+n = int(input())
+k = int(input())
+if n > k:
+    print('NO')
+if k > n:
+    print('YES')
+if n == k:
+    print('Dont know')
+  Вид треугольника
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b == c:
+    print('Равносторонний')
+if a == b or b == c or c == a:
+    print('Равнобедренный')
+if a != b != c:
+    print('Разносторонний')
+  Среднее число
+a = int(input())
+b = int(input())
+c = int(input())
+x = (a + b + c)//3
+print(x)
+  Церемония взвешивания
+a = int(input())
+if a < 60:
+    print('Легкий вес')
+if 60 <= a < 64:
+    print('Первый полусредний вес')
+if 64 <= a < 69:
+    print('Полусредний вес')
+  Количество дней
+a = int(input())
+if a == 1 or a == 3 or a == 5 or a == 7 or a == 8 or a == 10 or a == 12:
+    print('31')
+if a == 2:
+    print('29')
+if a == 4 or a == 9 or a == 11 or a == 6:
+    print('30')
